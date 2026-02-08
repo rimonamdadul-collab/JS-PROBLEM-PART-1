2cbbd4c71f33cca8f537043d162d75689d814bc6
rimonamdadul-collab <rimonamdadul@gmail.com>
2026-02-08 13:22:10 +0600

    add an the problem

---
+++ convert.js
@@ -0,0 +1,12 @@
+function inchiTofeet(inch){
+    const inchi = inch / 12;
+    return inchi
+}
+const shubohight = inchiTofeet(90)
+console.log(shubohight)
+function mailto(mailo){
+    const maile = mailo *1.60934;
+    return maile; 
+}
+const mailemiter = mailto(2)
+console.log(mailemiter)
